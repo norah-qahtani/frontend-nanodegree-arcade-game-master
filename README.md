55cefb169e830b97bb3644e868f2909fb6cb8909
Nora Al-qahtani <nora1991kjc@gmail.com>
2019-04-23 16:06:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Classic Arcade Game Clone Project
+
+usage notes
+-------------------------------------------------------------------
+-this application is Arcade Game Clone, where you would try to cross from the grass area to the water area passing by the enemy zone , you must try to reach the water area without allowing the enemies to kill you.  
+-when you reach the water area the game will tell you that you won Otherwise you must keep trying, when you are killed by the enemies the game will restart.
+-to be able to move or play you must use the keyboard directional keys (up, down ,left , right). 
+
+Installation
+-------------------------------------------------------------------
+-this game does not require installation.
+-you must run the project folder or copy the path of the html page and paste it in the internet browser to use the game.
+
+Developer info
+-------------------------------------------------------------------
+Phone: 000-333-4444
+Website:meme.com
+E-mail:student@gamil.com
+
+   
+
---
+++ js/app.js
@@ -0,0 +1,95 @@
+// Enemies our player must avoid
+var Enemy = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+    this.x =this.x+this.speed*dt;
+    if(this.x > 510) {
+        this.x = -50;
+        this.speed = 100 + Math.floor(Math.random() * 222);
+    }
+    // when the palyer touches the enemy restart   
+    if(player.x <= this.x + 80 &&
+       player.x +80 >= this.x &&
+       player.y <= this.y + 60 &&
+       60  + player.y >= this.y){
+        player.x = 202;
+        player.y = 405;
+    }
+    
+    
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(x,y)    {
+    this.x = x;
+    this.y = y;
+    this.player='images/char-boy.png';
+}
+
+Player.prototype.update = function(dt) {
+}
+
+Player.prototype.render = function(dt) {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(keyPress){
+    if(keyPress == 'left' && this.x > 0){
+        this.x -= 102;
+    }
+    if(keyPress == 'right' && this.x < 405){
+        this.x += 102;
+    }
+    if(keyPress == 'up' && this.y > 0){
+        this.y -= 83;
+    }
+    if(keyPress == 'down' && this.y < 405){
+        this.y += 83;
+    }
+    if(this.y<0){
+        setTimeout(function(){
+            player.x=202;
+            player.y=405;
+            alert('     you won!!    ');
+        }, 200);
+        
+    }
+}
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies =[];
+var enemyLocation =[63, 147, 230];
+
+enemyLocation.forEach(function (locationY){
+    enemy = new Enemy(0, locationY, 200);
+    allEnemies.push(enemy);
+});
+
+var player = new Player(202, 405);
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
